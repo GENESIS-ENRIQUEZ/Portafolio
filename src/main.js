157def50378ed56e9f77b1ee6a852532b80fb8b9
GENESIS-ENRIQUEZ <egenesis.egonzalez@gmail.com>
2019-05-22 10:18:56 -0500

    Testing

---
+++ src/main.js
@@ -0,0 +1,26 @@
+const listOfMethods =['Agile', 'Human Centered Design', 'Design Thinking', 'Research', 'User Experience Design'];
+const loopOfText = document.getElementById("loopText");
+
+// const drawText = (arrayListOfMethods) => {
+//     const sectionLoop = loopOfText;
+//     sectionLoop.innerHTML = "";
+//     arrayListOfMethods.map((listOfMethods) =>{
+//         sectionLoop.innerHTML += `
+//         `
+//     })
+// }
+
+listOfMethods.forEach(element => {
+    console.log(element);
+});
+
+
+
+// En el h3 quiero iterar el arrar listOfMethhods para que cada 5 segundos aparezca cada el contenido
+// de este array 
+// H3 con ID
+// Recorrer el array
+// Que pasen 5 segundos y aparezca el siguiente
+// Sobreeescibir el h3
+// Que sea un loop
+
