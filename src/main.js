35da2a210ce06c32610f4d466154a13e91e6714d
GENESIS-ENRIQUEZ <egenesis.egonzalez@gmail.com>
2019-05-27 10:02:28 -0500

    este commit representa los cambios del proyecto iterado

---
+++ src/main.js
@@ -0,0 +1,37 @@
+const listOfMethods =['Agile', 'Human Centered Design', 'Design Thinking', 'Research', 'User Experience Design'];
+const loopOfText = document.getElementById("loopText");
+
+// const drawText = (arrayListOfMethods) => {
+//     const sectionLoop = loopOfText;
+//     sectionLoop.innerHTML = "";
+//     arrayListOfMethods.map((listOfMethods) =>{
+//         sectionLoop.innerHTML += `
+//         `
+//     })
+// }
+
+
+count = 0
+const printMethods = () => {
+    //for(let count = 0; i < listOfMethods.length; i++) {
+        loopOfText.innerHTML = listOfMethods[count]
+        count ++
+        if(count > 4){
+            count = 0
+        }
+    //}
+}
+setInterval(() =>{
+    printMethods();
+},1600)
+
+
+
+// En el h3 quiero iterar el arrar listOfMethhods para que cada 5 segundos aparezca cada el contenido
+// de este array 
+// H3 con ID
+// Recorrer el array
+// Que pasen 5 segundos y aparezca el siguiente
+// Sobreeescibir el h3
+// Que sea un loop
+
